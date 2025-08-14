fad5f1cd2daa8617c15913a3b57fdfcd298c01dc
Samuel Chow <samuel.chow.45@gmail.com>
2025-08-14 12:31:06 -0400

    simple http

--- src/server.c
+++ src/server.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 
 int main() {
-	// define server response
-	char server_message[256] = "This is a response from the server!\n";
-	
+	FILE *html_data;
+	html_data = fopen("www/index.html", "r");
+
+	if (html_data == NULL) {
+		printf("Error opening file.\n");
+		return 1;
+	}
+
+	char buf[1024];
+	char http_headers[2048] = "HTTP/1.1 200 OK\r\n\n";
+
+	while (fgets(buf, sizeof(buf), html_data) != NULL) {
+		strcat(http_headers, buf);
+	}
+
+	fclose(html_data); 
+
 	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 	
 	struct sockaddr_in server_address;
@@ -22,15 +37,17 @@ int main() {
 	// listen for connections	
 	listen(server_socket, 10); // 2nd arg is max backlog 
 
+	int client_socket;
+
 	while (1) {
-		int client_socket = accept(server_socket, NULL, NULL);
+		client_socket = accept(server_socket, NULL, NULL);
 		
 		char buffer[1024] = {0};
 		read(client_socket, buffer, 1024);
 		printf("Received: %s\n", buffer);
 
 	
-		send(client_socket, server_message, sizeof(server_message), 0);
+		send(client_socket, http_headers, sizeof(http_headers), 0);
 		close(client_socket);
 	}
 	
